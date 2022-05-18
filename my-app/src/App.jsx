1792575522863f59f85a3d802bd4bdd06a6eff46
Rafaela <rafaelaperes215@gmail.com>
2022-05-18 20:28:15 -0300

    Comunicação Direta: Pai para filho

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -4,13 +4,19 @@ import React from 'react';
 import TabelaProdutos from './components/repeticao/TabelaProdutos';
 import ParOuImpar from './components/condicional/ParOuImpar';
 import UsuarioInfo from './components/condicional/UsuarioInfo';
+import DiretaPai from './components/comunicacao/DiretaPai';
 
 function App() {
   return (
     <div className="App">
 
-      <UsuarioInfo usuario={{ nome: 'Fernando'  }}></UsuarioInfo>
-      <UsuarioInfo usuario={{ email: 'fernando@exemplo.com'  }}></UsuarioInfo>
+    {/* // Comunicação Direta */}
+    <DiretaPai></DiretaPai>
+
+    {/* //////////////////////////////////////////////////////////////////////////// */}
+
+      {/* <UsuarioInfo usuario={{ nome: 'Fernando'  }}></UsuarioInfo>
+      <UsuarioInfo usuario={{ email: 'fernando@exemplo.com'  }}></UsuarioInfo> */}
 
       {/* <ParOuImpar numero={21}></ParOuImpar> */}
       
